4221bfc4a902fc9866b24e5d02615d2f3d3c525d
Shinichiro Oba <ooba@bricklife.com>
2016-05-26 14:04:42 +0900

    Add an activity indicator

--- APIKit-iOS7/ViewController.swift
+++ APIKit-iOS7/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +21,11 @@ class ViewController: UIViewController {
         //: Step 4: Send request
         let request = GetRateLimitRequest()
         
+        activityIndicator.startAnimating()
+        
         Session.sendRequest(request) { [weak self] result in
+            self?.activityIndicator.stopAnimating()
+            
             switch result {
             case .Success(let rateLimit):
                 self?.label.text = "count: \(rateLimit.count)\nreset: \(rateLimit.resetDate)"
